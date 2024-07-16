16ca65579ed2ecbb512ef73dbc5699528f894887
QiwangYu <qiwang.yu@tec-do.com>
2024-07-16 14:44:34 +0800

    new

---
+++ algorithm/array/array.go
@@ -0,0 +1,19 @@
+package array
+
+import "cmp"
+
+func BinarySearch[T cmp.Ordered](array []T, target T) int {
+	left := 0
+	right := len(array) - 1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if array[mid] == target {
+			return mid
+		} else if array[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
+}
