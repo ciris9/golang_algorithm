ab4a2cde0fc7a55c435c9a5f5a378f385ffa97eb
cirth9 <2311975835@qq.com>
2024-07-22 00:00:42 +0800

    feat:new

--- coding/leetcode/two/main.go
+++ coding/leetcode/two/main.go
@@ -2,6 +2,7 @@ package two
 
 import (
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -49,4 +50,104 @@ func caculate(nums []int) int {
 
 func strStr(haystack string, needle string) int {
 	return strings.Index(haystack, needle)
+}
+
+func myAtoi(s string) int {
+	trimLefts := strings.TrimLeft(s, " ")
+	if trimLefts == "" {
+		return 0
+	}
+	var isBelowZero bool
+	if trimLefts[0] == '-' {
+		isBelowZero = true
+		trimLefts = trimLefts[1:]
+	} else if trimLefts[0] == '+' {
+		isBelowZero = false
+		trimLefts = trimLefts[1:]
+	}
+	if trimLefts == "" {
+		return 0
+	}
+	trimLefts = strings.TrimLeft(trimLefts, "0")
+	for index, trimLeft := range trimLefts {
+		if trimLeft < '0' || trimLeft > '9' {
+			trimLefts = trimLefts[:index]
+			break
+		}
+	}
+	i, _ := strconv.ParseInt(trimLefts, 10, 64)
+	if isBelowZero {
+		i *= -1
+	}
+	if i > (1<<31 - 1) {
+		return 1<<31 - 1
+	} else if i < -(1 << 31) {
+		return -(1 << 31)
+	}
+	return int(i)
+}
+
+func jump(nums []int) int {
+	step, maxIndex, end := 0, 0, 0
+	for i := 0; i < len(nums)-1; i++ {
+		maxIndex = max(maxIndex, i+nums[i])
+		if end == i {
+			end = maxIndex
+			step++
+		}
+	}
+	return step
+}
+
+func climbStairs(n int) int {
+	dp := make([]int, n+1)
+	dp[1] = 0
+	dp[2] = 1
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
+func rob(nums []int) int {
+	dp := make([]int, len(nums))
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(nums[i-2]+nums[i], dp[i-1])
+	}
+	return dp[len(nums)-1]
+}
+
+// 假设对于某个数n来说，他的最小数量是m，那么对于n+1来讲就是
+func numSquares(n int) int {
+	f := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		minn := math.MaxInt32
+		for j := 1; j*j <= i; j++ {
+			minn = min(minn, f[i-j*j])
+		}
+		f[i] = minn + 1
+	}
+	return f[n]
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	first, second := head, head.Next
+	for first.Next != nil && second.Next != nil && first.Next.Next != nil && second.Next.Next.Next != nil {
+		nextFirst := first.Next.Next
+		nextSecond := second.Next.Next
+		first.Next = nextFirst
+		second.Next = first
+		first, second = nextSecond, nextFirst
+	}
+	return head.Next
 }
---
+++ data_structure/trie/tire.go
@@ -0,0 +1,45 @@
+package trie
+
+const charSize = 26
+
+type Trie struct {
+	children [charSize]*Trie
+	isEnd    bool
+}
+
+func New() Trie {
+	return Trie{}
+}
+
+func (t *Trie) Insert(word string) {
+	node := t
+	for _, ch := range word {
+		ch -= 'a'
+		if node.children[ch] == nil {
+			node.children[ch] = &Trie{}
+		}
+		node = node.children[ch]
+	}
+	node.isEnd = true
+}
+
+func (t *Trie) SearchPrefix(prefix string) *Trie {
+	node := t
+	for _, ch := range prefix {
+		ch -= 'a'
+		if node.children[ch] == nil {
+			return nil
+		}
+		node = node.children[ch]
+	}
+	return node
+}
+
+func (t *Trie) Search(word string) bool {
+	node := t.SearchPrefix(word)
+	return node != nil && node.isEnd
+}
+
+func (t *Trie) StartsWith(prefix string) bool {
+	return t.SearchPrefix(prefix) != nil
+}
